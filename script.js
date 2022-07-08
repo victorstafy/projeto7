7bd0015500ec1e422de305c343edce552299702b
Victor <victorstafy11@gmail.com>
2022-07-07 23:59:14 -0300

    layout pronto

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// let window_width=window.innerWidth;
+// const max_desktop_w=1098;
+// const image_class=".imagem_direita";
+
+// if (window_width<=max_desktop_w){
+//     image_div=document.querySelector(image_class);
+//     image_div.style.display="none";
+// }
+// else{
+//     image_div.style.display="flex";
+// }
