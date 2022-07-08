f9e0422285150cd5914e7d4c60285be4879c6540
Victor <victorstafy11@gmail.com>
2022-07-08 12:00:58 -0300

    Problema descoberto no layout aparentemente resolvido, selecao de item para compra implementado

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
+let selecao_1=0; let selecao_2=0; let selecao_3=0; 
+let selecao_at_1=0; let selecao_at_2=0; let selecao_at_3=0; 
+let user_img;
 
-// let window_width=window.innerWidth;
-// const max_desktop_w=1098;
-// const image_class=".imagem_direita";
+const nome=prompt("Qual seu nome?");
 
-// if (window_width<=max_desktop_w){
-//     image_div=document.querySelector(image_class);
-//     image_div.style.display="none";
-// }
-// else{
-//     image_div.style.display="flex";
-// }
+function select_item1(element){ 
+    console.log(element)
+    if (element.classList.contains("selecionado")){
+        element.classList.remove("selecionado");
+        return
+    }
+    if (selecao_1){
+        selecao_1.classList.remove("selecionado");
+    }
+    element.classList.add("selecionado")
+    selecao_at_1="ativado";
+    selecao_1=element;
+    button_activate()
+}
+
+function select_item2(element){ 
+    console.log(element)
+    if (element.classList.contains("selecionado")){
+        element.classList.remove("selecionado");
+        return
+    }
+    if (selecao_2){
+        selecao_2.classList.remove("selecionado");
+    }
+    element.classList.add("selecionado")
+    selecao_at_2="ativado";
+    selecao_2=element;
+    button_activate()
+}
+
+function select_item3(element){ 
+    console.log(element)
+    if (element.classList.contains("selecionado")){
+        element.classList.remove("selecionado");
+        return
+    }
+    if (selecao_3){
+        selecao_3.classList.remove("selecionado");
+    }
+    element.classList.add("selecionado")
+    selecao_at_3="ativado";
+    selecao_3=element;
+    button_activate()
+}
+
+function button_activate(){
+    if (selecao_at_1==="ativado" && selecao_at_2==="ativado" && selecao_at_3==="ativado"){
+        el_finish=document.querySelector(".button");
+        el_finish.style.background='#404EED';
+    }
+}
+function get_user_image(){
+    user_img = document.querySelector("input").value;
+}
+
+function confirm_purchase(){
+    get_user_image()
+    alert("Compra confirmada! "+user_img)
+}
