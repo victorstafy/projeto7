3cc0dc3d52035b9a25c839b114cb14b4cfcafcbf
Victor <victorstafy11@gmail.com>
2022-07-09 17:43:51 -0300

    chamada da api funcionando, atualizando quando compra e enviada para o servidor, repeticao de compara anterior funcionando, autor nao e devolvido no objeto devido a problema da api, estado do sistema desativado apos realizacao de compra, margin-bottom do botao ampliada

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ let selecao_at_1=0; let selecao_at_2=0; let selecao_at_3=0;
 const urlAPI="https://mock-api.driven.com.br/api/v4/shirts-api/shirts"
 const n_last_request=10;
 
-const nome=prompt("Qual seu nome?");
+let nome=prompt("Qual seu nome?");
+while (!nome){
+    nome=prompt("Qual seu nome?");
+}
 update_last_requests()
 
 
@@ -50,9 +53,14 @@ function select_item3(element){
 }
 
 function button_activate(){
+    el_finish=document.querySelector(".button");
     if (selecao_at_1!==0 && selecao_at_2!==0 && selecao_at_3!==0){
-        el_finish=document.querySelector(".button");
         el_finish.style.background='#404EED'; 
+        el_finish.disabled=false;
+    }
+    else{
+        el_finish.style.background='#C4C4C4'; 
+        el_finish.disabled=true;
     }
 }
 function get_user_image(){
@@ -60,9 +68,20 @@ function get_user_image(){
     return user_img
 }
 
+function clear_state(){
+    document.querySelector("input").value="";
+    const selecionados=document.querySelectorAll(".selecionado");
+    for(let item of selecionados){
+        item.classList.remove("selecionado");
+    }
+    selecao_at_1=0; selecao_at_2=0; selecao_at_3=0; 
+    button_activate()
+    
+}
+
 function create_object(){
     user_img=get_user_image();
-    cloth_model={
+    let cloth_model={
         "model": selecao_at_1 ,
         "neck": selecao_at_2,
         "material": selecao_at_3,
@@ -77,6 +96,7 @@ function create_object(){
 
 function post_object(cloth_object) {
     const promise = axios.post(urlAPI, cloth_object);
+    console.log(cloth_object)
     promise.then(wite_request);
     promise.catch(show_error)
 }
@@ -84,16 +104,16 @@ function post_object(cloth_object) {
 function confirm_purchase(){
     let cloth_model=create_object();
     post_object(cloth_model);
-    update_last_requests();
 }
 
 function wite_request(response){
-    console.log(response)
     if (response.status===422){
         alert("Ops, não conseguimos processar sua encomenda!22")
     }
     else if (response.status===201){
         alert("Compra confirmada!")
+        update_last_requests();
+        clear_state()
     }
 }
 
@@ -108,6 +128,7 @@ function update_last_requests(){
 
 function write_recent_requests(response){
     prev_request_div=document.querySelector(".lista_pedidos");
+    prev_request_div.innerHTML="";
     for (let i=response.data.length-n_last_request; i<response.data.length; i++){
         create_request_div(prev_request_div,response.data[i]);
     }
@@ -116,28 +137,46 @@ function write_recent_requests(response){
 function create_request_div(parent_div,request_object){
 
     let div_id=request_object.id;
+
+    let model=request_object.model;
+    let neck=request_object.neck;
+    let material=request_object.material;
+
     let img_src=request_object.image;
     let subtitle=request_object.owner;
-    console.log(request_object)
+ 
+    div_id=[model,neck,material].join('_');
+    console.log(div_id)
+    parent_div.innerHTML+=`<div class='pedido_legenda' id=${div_id}  onclick=confirm_request(this)>
+        <div class='pedido'>
+            <img src=${img_src}>
+        </div>
+        <p class="bold">Criador:</p> ${subtitle}
+    </div>`;
+}
 
-    // Create main div
-    let div = document.createElement("div");
-    div.id = div_id ;
-    div.className = "pedido_legenda";
-    parent_div.appendChild(div);  
+function confirm_request(element){
+    console.log(element);
+    let id_info=element.id;
+    let id_info_array=id_info.split('_'); // array [model,neck,material]
+    let author=element.lastChild.textContent
+    author=author.substring(1, author.length - 5);
+    let img_src=element.querySelector("img").src
+    
+    user_confirmation=confirm("Você quer mesmo este item?");
+    console.log(id_info_array)
+    if (user_confirmation){
+        let cloth_model={
+            "model": id_info_array[0] ,
+            "neck": id_info_array[1],
+            "material": id_info_array[2],
+            "image": img_src,
+            "owner": nome,
+            "author":author,
+        };
+        console.log(cloth_model)
+        post_object(cloth_model)
+    }
 
-    // Create image div
-    let sub_div = document.createElement("div");
-    sub_div.className = "pedido";
-    div.append(sub_div);
 
-    // Create image
-    let el_img = document.createElement("img");
-    el_img.className = "pedido";
-    sub_div.appendChild(el_img);
-    el_img.src=img_src;
-
-    console.log(subtitle)
-    // Create subtitle div inside main div
-    div.innerHTML+=`<p class="bold">Criador:</p> ${subtitle}`
 }
